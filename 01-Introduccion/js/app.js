825450794f3bcaa9f752ca842e889eb72e35ad09
Richard <richardrojas38@gmail.com>
2024-01-15 22:13:28 -0500

    JavaScript File

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,24 @@
+//alert('Hola mundo');
+
+/* const nombre = prompt('¿Cual es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno`; */
+
+console.log("log");
+console.warn("warn");
+console.error("error");
+console.info("info");
+console.table("table");
+
+console.time("Hola");
+
+console.warn("warn");
+console.warn("warn");
+console.warn("warn");
+console.warn("warn");
+console.warn("warn");
+console.warn("warn");
+console.warn("warn");
+
+console.time("Hola");
+
+console.log({ nombre: juan });
