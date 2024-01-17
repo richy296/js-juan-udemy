f677326a787d896c8eb8d76e978f994642ec45c1
Richard <richardrojas38@gmail.com>
2024-01-16 21:09:38 -0500

    Variables type var and eslint config

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,33 @@
+// Iniciar una variable con un valor
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se puede reasignar
+producto = "Monitor de 19 pulgadas";
+console.log(producto);
+
+// Javascript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden incializar sin valor y asignarlo despues
+var disponible;
+console.log(disponible);
+disponible = true;
+disponible = false;
+console.log(disponible);
+
+// Inicializar múltiples variables
+var categoria = "Computadoras",
+  marca = "Marca Famosa",
+  calificaciones = 5;
+
+console.log({categoria, marca, calificaciones});
+
+// CamelCase
+var isFalsy = false;
+// SnakeCase
+var user_year = 1992;
+// Pascal
+var FirstName = 'Richard';
+
